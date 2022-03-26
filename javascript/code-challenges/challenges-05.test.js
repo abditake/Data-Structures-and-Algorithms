84aa79856a81332ab6ac01bd91c81eb8475cf615
Abdinasir Yussuf <Abdinasir.yussuf@outlook.com>
2022-03-25 21:41:27 -0700

    lab 5

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -128,9 +128,7 @@ Note: You must use reduce for this challenge. You may not use the built-in .reve
 ------------------------------------------------------------------------------------------------ */
 
 const reversedString = (str) => {
-    str.reduce((acc,current) => {
-      acc + current,''
-    } );
+  return str.split('').reduce((a, b) => b + a, '');
 };
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
@@ -257,9 +255,11 @@ const snorlaxData = {
   weight: 4600,
 };
 
-const extractStat = (statName, arr) => {
-  // Solution code here...
-};
+// const extractStat = (str) => {
+//     let arr = str.split('');
+//     let cool = arr.reduce((previous,current) => 
+//       return current + previous; ,''
+// };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 11 - Stretch Goal
