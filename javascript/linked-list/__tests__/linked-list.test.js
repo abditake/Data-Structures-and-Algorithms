638c7f5155e956fdf52c9417db856adf875da93f
Abdinasir Yussuf <Abdinasir.yussuf@outlook.com>
2022-06-19 23:57:12 -0700

    linked list

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,58 @@
 'use strict';
 
-// Require our linked list implementation
-const LinkedLilst = require('../index');
+let { LinkedList, Node } = require('../index');
 
-describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+describe('Testing Linked List', () => {
+  test('should create linked list ', () => {
+    let list = new LinkedList();
+    expect(list).toBeTruthy();
+  });
+
+  test('can insert value into linked list', () => {
+    let node = new Node(6);
+    let list = new LinkedList();
+    list.head = node;
+    expect(list.head).toEqual(node);
+  });
+
+  test('head property properly points to head node', () => {
+    let list = new LinkedList();
+    l.head = new Node(10);
+    l.head.next = new Node(11);
+    expect(l.head.val).toEqual(10);
+  });
+
+  test('head property properly append', () => {
+    let l = new LinkedList();
+    l.head = new Node(10);
+    l.head.next = new Node(11);
+    l.append(12);
+    expect(l.head.next.next.val).toEqual(12);
+  });
+
+  test('head property properly insertBefore', () => {
+    let l = new LinkedList();
+    l.head = new Node(10);
+    l.head.next = new Node(11);
+    l.insertBefore(11, 8);
+    expect(l.head.next.val).toEqual(8);
+  });
+
+  test('head property properly insertAfter', () => {
+    let l = new LinkedList();
+    l.head = new Node(10);
+    l.head.next = new Node(11);
+    l.insertAfter(11, 8);
+    expect(l.head.next.next.val).toEqual(8);
+  });
+
+  test('can properly insert multiple nodes', () => {
+    let l = new LinkedList();
+    l.head = new Node(1);
+    l.insertHead(2);
+    l.insertHead(3);
+    l.insertHead(4);
+    l.insertHead(5);
+    expect(l.head.next.next.val).toEqual(3);
   });
 });
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,66 @@
 'use strict';
 
-class LinkedList {
-
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next;
+  }
 }
 
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+
+  traverse() {
+    let current = this.head;
+
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+
+  add(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = node;
+  }
+}
+
+const linkedList = new LinkedList();
+
+
+
+const nodeA = new Node('a');
+const nodeB = new Node('b');
+const nodeC = new Node('c');
+const nodeD = new Node('d');
+
+linkedList.head = nodeA;
+
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(3);
+linkedList.add(4);
+linkedList.add(5);
+linkedList.add(6);
+
+
+linkedList.traverse();
+
+console.log(linkedList);
+
 module.exports = LinkedList;
+module.exports = Node;
