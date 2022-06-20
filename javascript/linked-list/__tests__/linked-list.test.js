031b4bd51c0da92280bd3a252e457f5a4700b193
Abdinasir Yussuf <Abdinasir.yussuf@outlook.com>
2022-06-20 00:35:29 -0700

    tests

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -8,51 +8,17 @@ describe('Testing Linked List', () => {
     expect(list).toBeTruthy();
   });
 
-  test('can insert value into linked list', () => {
+  test('can Add value into linked list', () => {
     let node = new Node(6);
     let list = new LinkedList();
     list.head = node;
     expect(list.head).toEqual(node);
   });
 
-  test('head property properly points to head node', () => {
+  test('head property points to head node', () => {
     let list = new LinkedList();
-    l.head = new Node(10);
-    l.head.next = new Node(11);
-    expect(l.head.val).toEqual(10);
+    list.head = new Node(10);
+    list.head.next = new Node(11);
+    expect(list.head.val).toEqual(10);
   });
-
-  test('head property properly append', () => {
-    let l = new LinkedList();
-    l.head = new Node(10);
-    l.head.next = new Node(11);
-    l.append(12);
-    expect(l.head.next.next.val).toEqual(12);
-  });
-
-  test('head property properly insertBefore', () => {
-    let l = new LinkedList();
-    l.head = new Node(10);
-    l.head.next = new Node(11);
-    l.insertBefore(11, 8);
-    expect(l.head.next.val).toEqual(8);
-  });
-
-  test('head property properly insertAfter', () => {
-    let l = new LinkedList();
-    l.head = new Node(10);
-    l.head.next = new Node(11);
-    l.insertAfter(11, 8);
-    expect(l.head.next.next.val).toEqual(8);
-  });
-
-  test('can properly insert multiple nodes', () => {
-    let l = new LinkedList();
-    l.head = new Node(1);
-    l.insertHead(2);
-    l.insertHead(3);
-    l.insertHead(4);
-    l.insertHead(5);
-    expect(l.head.next.next.val).toEqual(3);
-  });
-});
+})
