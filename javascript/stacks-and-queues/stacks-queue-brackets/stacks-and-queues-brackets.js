be06b64b92b973990dd85ca53d7c8917d990900a
Abdinasir Yussuf <Abdinasir.yussuf@outlook.com>
2022-06-30 09:03:58 -0700

    tweakin

---
+++ javascript/stacks-and-queues/stacks-queue-brackets/stacks-and-queues-brackets.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const { Stack } = require('../stacks-and-queues');
+
+// 
+class Brackets{
+
+
+  constructor(){
+    this.stack = new Stack;
+  }
+
+  // still working this out I got help 
+  
+  isBracketValid = (brackets) => {
+    if(brackets.length % 2 !== 0){
+      return false;
+    }
+    const brackets = {
+      '[': ']',
+      '(': ')',
+      '{': '}'
+    };
+    for (const bracket of brackets) {
+  
+     if(map.hasOwnProperty(bracket)) {
+       this.stack.push(bracket)
+     } else {
+       
+   }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const { Stack } = require('../stacks-and-queues');
+
+// help through leetcode
+// isBracketsValid(bracket){
+
+//       let stack = new Stack;
+
+//   for (let i = 0; i < bracket.length; i++) {
+//     let c = bracket.charAt(i);
+//     switch (c) {
+//       case '(': stack.push(')');
+//         break;
+//       case '[': stack.push(']');
+//         break;
+//       case '{': stack.push('}');
+//         break;
+//       default:
+//         if (c !== stack.pop()) {
+//           return false;
+//         }
+//     }
+//   }
+
+//   return stack.length === 0;
+// }
+
+
+
+
+
