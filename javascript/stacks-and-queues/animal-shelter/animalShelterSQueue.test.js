ddf4caa631c954f7270313747cf74f7dcec69af2
Abdinasir Yussuf <Abdinasir.yussuf@outlook.com>
2022-06-29 23:52:20 -0700

    all the tests so far

--- javascript/stacks-and-queues/animal-shelter/animalShelterSQueue.test.js
+++ javascript/stacks-and-queues/animal-shelter/animalShelterSQueue.test.js
@@ -1,24 +1,36 @@
 'use strict';
 
 
-const AnimalShelter = require('./stack-queue-animal-shelter');
+const { AnimalShelter, Cat, Dog } = require('./stack-queue-animal-shelter');
 
 
 describe('Animal Shelter Queue Tests', () => {
   test('Check if Queues are setup', () => {
-    const aQueue = new AnimalShelter();
-    expect(aQueue.dog.front).toEqual(null);
-    expect(aQueue.dog.back).toEqual(null);
-    expect(aQueue.cat.front).toEqual(null);
-    expect(aQueue.cat.back).toEqual(null);
+    const animalQueue = new AnimalShelter();
+    console.log(animalQueue.cat.front);
+    expect(animalQueue.cat.front).toEqual(null);
+    expect(animalQueue.cat.back).toEqual(null);
+    expect(animalQueue.dog.front).toEqual(null);
+    expect(animalQueue.dog.back).toEqual(null);
   });
   test('can add cats or dogs to a queue', () => {
-    const aQueue = new AnimalShelter();
-
+    const animalQueue = new AnimalShelter();
+    let dog1 = new Dog('goodBoy');
+    let cat1 = new Cat('goodCat');
+    animalQueue.enqueue(dog1);
+    animalQueue.enqueue(cat1);
+    expect(animalQueue.cat).not.toEqual(null);
+    expect(animalQueue.dog).not.toEqual(null);
   });
   test('can remove cats or dogs to a queue', () => {
-    const aQueue = new AnimalShelter();
-    
+    const animalQueue = new AnimalShelter();
+    let dog1 = new Dog('goodBoy');
+    let cat1 = new Cat('goodCat');
+    animalQueue.enqueue(dog1);
+    animalQueue.enqueue(cat1);
+    animalQueue.dequeue(dog1);
+    animalQueue.dequeue(cat1);
+    expect(animalQueue.cat)
   });
 
   test('should error if animal to enqueue is not of type Dog or Cat and log a message of `Sorry, we cant take them`', () => {
