a2ccaad298f79c6a0dc4d74e78de60342bef5a02
Abdinasir Yussuf <Abdinasir.yussuf@outlook.com>
2022-07-07 18:14:59 -0700

    readme;

--- javascript/trees/tree-breadth-first/index.js
+++ javascript/trees/tree-breadth-first/index.js
@@ -1,54 +1,58 @@
-'use strict';
-
-let { Queue } = require('../../stacks-and-queues/stacks-and-queues')
-
+"use strict";
 
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 
 class BinaryTree {
-    constructor() {
-        this.root = null;
-    }
-
-
-    breadthFirst(tree) {
-        let arr = [];
-        let queue = new Queue();
-        let current = tree.root;
-        queue.enqueue(current);
-
-        while (queue > 0) {
-            let front = queue.dequeue(current);
-            arr.push(front.value);
-            if (current.left) queue.enqueue(current.left);
-            if (current.right) queue.enqueue(current.right);
-        }
-        return arr;
-    }
+  constructor() {
+    this.root = null;
+  }
 }
 
 
+function breadthFirstSearch(tree){
+  let queue = [];
+  let result = [];
+  let current = null;
+
+  queue.push(tree.root);
+
+  while (queue.length > 0) {
+    current = queue.shift();
+    result.push(current.value);
+    if (current.left) {
+      queue.push(current.left);
+    }
+    if (current.right) {
+      queue.push(current.right);
+    }
+  }
+  console.log(result);
+}
+
 
 let tree = new BinaryTree();
-tree.root = new Node(10);
-tree.root.left = new Node(5);
-tree.root.right = new Node(15);
 
 
 
+tree.root = new Node(1);
+tree.root.left = new Node(2);
+tree.root.right = new Node(3);
+tree.root.left.left = new Node(4);
 
 
-console.log(queue);
+tree.root.left.right = new Node(5);
+tree.root.right.left = new Node(6);
+tree.root.right.right = new Node(7);
 
 
-
+breadthFirstSearch(tree);
 
 
 
---
+++ javascript/trees/tree-breadth-first/readme.md
@@ -0,0 +1,15 @@
+#Breadth-First Traversal
+
+### Challenge Summary
+
+-  Write a function called breadth first
+     - Arguments: tree
+     - Return: list of all values in the tree, in the order they were encountered
+### Approach & Efficiency
+
+- There is queue like behaviour but using an array seemed so much easier. 
+
+- Add all node values at each level starting from root to an array then return that array
+
+### Whiteboard Process
+![Algorithm](../img/Algo.png)
